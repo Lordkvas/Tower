b61e1980555c17cf9d6246cb69fce49b009324c7
Lordkvas <145777821+Lordkvas@users.noreply.github.com>
2023-09-22 18:26:58 +0400

    Update main Башня.cpp

--- main Башня.cpp
+++ main Башня.cpp
@@ -1,12 +1,27 @@
 #include "TXLib.h"
+
 class Bashni
 {
 public:
+    HDC pic;
+    COLORREF color=TX_RED,
     int x, y;
-    int Draw (int x1,int y1)
+    
+    Bashni(int x1,int y1)
+     
     {
-    x=x1
-    y=y1
+    x=x1;
+    y=y1;
+    pic=txLoadImage()
+    
+    
+    };
+    
+    
+    int Draw ()
+    {
+    txTransparentBlt(txDC(),x,y, pic,0,0, cvet);
+    
     }
 
 };
