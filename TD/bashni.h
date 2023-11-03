2c12e869e3ce785004cc4c3b1068b8521b158c7b
Lordkvas <145777821+Lordkvas@users.noreply.github.com>
2023-11-03 18:31:07 +0400

    Add files via upload

---
+++ TD/bashni.h
@@ -0,0 +1,78 @@
+#include "TXLib.h"
+
+class Bashni
+{
+public:
+    HDC pic;
+    COLORREF color=TX_RED,cvet=TX_WHITE;
+    int x, y, w=80, h=92;
+
+    Bashni(int x1,int y1, string file)
+
+    {
+    x=x1;
+    y=y1;
+    pic=txLoadImage(file.c_str());
+
+
+    };
+
+
+    int Draw ()
+    {
+    txTransparentBlt (txDC(),x,y,w,h, pic, 0,0, cvet);
+
+};
+
+};
+
+
+
+
+
+ class Snaryad
+{
+public:
+
+
+    HDC pic;
+    int finish=0;
+    COLORREF cvet=TX_WHITE;
+
+
+
+
+
+    int x, y, w=27, h=26;
+    Snaryad(int x1,int y1, string file)
+    {
+    x=x1;
+    y=y1;
+    pic=txLoadImage(file.c_str());
+
+    };
+
+
+    int Move(int x1, int y1)
+    {
+    if (x1>x) x=x+4;
+    if (x1<x) x=x-4;
+    if (y1>y) y=y+4;
+    if (y1<y) y=y-4;
+
+
+ if (x<x1+4&&x>x1-4&&y<y1+4&&y>y1-4) finish=1;
+
+
+    };
+
+
+
+    int Draw ()
+    {
+    txTransparentBlt (txDC(),x,y,w,h, pic, 0,0, cvet);
+
+};
+
+};
+
